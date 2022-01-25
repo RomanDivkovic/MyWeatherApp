178f4f06ee0537189d4b96e4d9d12b17a428e66a
RomanDivkovic <divkovicroman@gmail.com>
2022-01-25 12:47:20 +0100

    Added background photo

--- src/Screens/SearchScreen.js
+++ src/Screens/SearchScreen.js
@@ -6,7 +6,8 @@ import {
   Image,
   ScrollView,
   Alert,
-  FlatList
+  FlatList,
+  ImageBackground
 } from 'react-native'
 import CustomButton from '../components/customButton'
 import CustomInput from '../components/customInput'
@@ -14,168 +15,187 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import api from '../Utils/api/api'
 import api_key from '../components/apiKey'
 
-export default function SearchScreen() {
+export default function SearchScreen({ navigation }) {
   const [result, setResult] = useState([])
   const [text, onTextChange] = useState('')
   const [loaded, setLoaded] = useState(true)
 
-  if (loaded === true) {
+  const image = {
+    uri: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_960_720.jpg'
+  }
+
+  if (loaded === true || result.city.name === undefined) {
     return (
-      <ScrollView>
-        <SafeAreaView>
-          <CustomInput onChangeText={onTextChange} value={text} />
-          <CustomButton
-            title="Search"
-            onPress={() => {
-              const searchApi = async () => {
-                try {
-                  const response = await api.get(
-                    `forecast?q=${text}&units=metric&appid=${api_key}`
-                  )
-                  setResult(response.data)
-                  setLoaded(false)
-                } catch (error) {
-                  setResult(error.toString())
-                  console.log('Error Result from api call', result)
-                  Alert(result)
-                }
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <CustomInput onChangeText={onTextChange} value={text} />
+        <CustomButton
+          title="Search"
+          onPress={() => {
+            const searchApi = async () => {
+              try {
+                const response = await api.get(
+                  `forecast?q=${text}&units=metric&appid=${api_key}`
+                )
+                setResult(response.data)
+                setLoaded(false)
+              } catch (error) {
+                setResult(error.toString())
+                console.log('Error Result from api call', result)
+                alert(result)
               }
-              searchApi()
-            }}
-          />
-        </SafeAreaView>
-      </ScrollView>
+            }
+            searchApi()
+          }}
+        />
+      </ImageBackground>
     )
   } else {
     return (
-      <View style={{ height: '100%' }}>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
         <ScrollView>
-          <SafeAreaView>
-            <CustomInput onChangeText={onTextChange} value={text} />
-            <View style={styles.container}>
-              <Text style={textStyles.Country}>
-                {result.city.name}, {result.city.country}
-              </Text>
-              <Text>{result.city.sunrise}</Text>
-              <Text>{result.city.sunset}</Text>
-              <Text
-                style={{
-                  fontSize: 22,
-                  fontWeight: '700',
-                  shadowOffset: {
-                    width: 5,
-                    height: 5
-                  },
-                  shadowOpacity: 0.4,
-                  shadowRadius: 2
-                }}
-              >
-                {result.list[0].main.temp}ºC
-              </Text>
-              <Text tyle={textStyles.feelsLike}>
-                {result.list[0].main.feels_like}ºC
-              </Text>
+          <CustomInput onChangeText={onTextChange} value={text} />
+          <View style={styles.container}>
+            <Text style={textStyles.Country}>
+              {result.city.name}, {result.city.country}
+            </Text>
+            {/* <Text>{result.city.sunrise}</Text>
+            <Text>{result.city.sunset}</Text> */}
+            <Text
+              style={{
+                fontSize: 22,
+                color: 'white',
+                fontWeight: '700',
+                shadowOffset: {
+                  width: 5,
+                  height: 5
+                },
+                shadowOpacity: 0.4,
+                shadowRadius: 2
+              }}
+            >
+              {result.list[0].main.temp}ºC
+            </Text>
+            <Text style={textStyles.feelsLike}>
+              {result.list[0].main.feels_like}ºC
+            </Text>
 
-              <Text style={styles.main}>{result.list[0].weather[0].main}</Text>
-              <Text style={styles.description}>
-                {result.list[0].weather[0].description}
-              </Text>
-              <Image
-                style={styles.pic}
-                source={{
-                  uri: `http://openweathermap.org/img/w/${result.list[0].weather[0].icon}.png`
-                }}
-              />
-              <Text style={styles.max}>
-                Max: {result.list[0].main.temp_max}ºC
-              </Text>
-              <Text style={styles.min}>
-                Min: {result.list[0].main.temp_min}ºC
-              </Text>
-              <Text>Humidity: {result.list[0].main.humidity}</Text>
-              <Text>Visibility: {result.list[0].visibility}</Text>
-            </View>
-
-            <View style={styles.viewList}>
-              <View style={styles.eachList}>
-                <FlatList
-                  data={result.list}
-                  horizontal
-                  renderItem={({ item }) => (
-                    <View style={styles.item}>
-                      <Text style={{ fontFamily: 'Montserrat1' }}>
-                        {item.dt_txt}
-                      </Text>
-                      <Image
-                        style={styles.pic}
-                        source={{
-                          uri: `http://openweathermap.org/img/w/${item.weather[0].icon}.png`
-                        }}
-                      />
-                      <Text
-                        style={{
-                          alignSelf: 'center',
-                          fontFamily: 'Montserrat2'
-                        }}
-                      >
-                        {item.weather[0].description}
-                      </Text>
-                      <Text
-                        style={{
-                          fontSize: 15,
-                          fontFamily: 'Montserrat3',
-                          alignSelf: 'center'
-                        }}
-                      >
-                        Current: {item.main.temp}ºC
-                      </Text>
-                      <Text
-                        style={{
-                          fontSize: 15,
-                          fontFamily: 'Montserrat3',
-                          alignSelf: 'center'
-                        }}
-                      >
-                        Feels like: {item.main.feels_like}ºC
-                      </Text>
-                      <Text
-                        style={{
-                          fontSize: 15,
-                          fontFamily: 'Montserrat3',
-                          alignSelf: 'center'
-                        }}
-                      >
-                        Min: {item.main.temp_min}ºC
-                      </Text>
-                      <Text
-                        style={{
-                          fontSize: 15,
-                          fontFamily: 'Montserrat3',
-                          alignSelf: 'center'
-                        }}
-                      >
-                        Max: {item.main.temp_max}ºC
-                      </Text>
-                    </View>
-                  )}
-                  keyExtractor={(weather) => weather.dt_txt.toString()}
-                />
-              </View>
-            </View>
-
-            <CustomButton
-              title="Search"
-              onPress={() => {
-                getWeather(text)
+            <Text style={textStyles.main}>
+              {result.list[0].weather[0].main}
+            </Text>
+            <Text style={textStyles.description}>
+              {result.list[0].weather[0].description}
+            </Text>
+            <Image
+              style={styles.pic}
+              source={{
+                uri: `http://openweathermap.org/img/w/${result.list[0].weather[0].icon}.png`
               }}
             />
-          </SafeAreaView>
+            <Text style={textStyles.max}>
+              Max: {result.list[0].main.temp_max}ºC
+            </Text>
+            <Text style={textStyles.min}>
+              Min: {result.list[0].main.temp_min}ºC
+            </Text>
+            <Text style={textStyles.humidity}>
+              Humidity: {result.list[0].main.humidity}
+            </Text>
+            <Text style={textStyles.visibility}>
+              Visibility: {result.list[0].visibility}
+            </Text>
+          </View>
+
+          <View style={styles.eachList}>
+            <FlatList
+              data={result.list}
+              horizontal
+              renderItem={({ item }) => (
+                <View style={styles.item}>
+                  <Text style={{ fontFamily: 'Montserrat1', color: 'white' }}>
+                    {item.dt_txt}
+                  </Text>
+                  <Image
+                    style={styles.pic}
+                    source={{
+                      uri: `http://openweathermap.org/img/w/${item.weather[0].icon}.png`
+                    }}
+                  />
+                  <Text
+                    style={{
+                      color: 'white',
+                      alignSelf: 'center',
+                      fontFamily: 'Montserrat2'
+                    }}
+                  >
+                    {item.weather[0].description}
+                  </Text>
+                  <Text
+                    style={{
+                      color: 'white',
+                      fontSize: 15,
+                      fontFamily: 'Montserrat3',
+                      alignSelf: 'center'
+                    }}
+                  >
+                    Current: {item.main.temp}ºC
+                  </Text>
+                  <Text
+                    style={{
+                      color: 'white',
+                      fontSize: 15,
+                      fontFamily: 'Montserrat3',
+                      alignSelf: 'center'
+                    }}
+                  >
+                    Feels like: {item.main.feels_like}ºC
+                  </Text>
+                  <Text
+                    style={{
+                      color: 'white',
+                      fontSize: 15,
+                      fontFamily: 'Montserrat3',
+                      alignSelf: 'center'
+                    }}
+                  >
+                    Min: {item.main.temp_min}ºC
+                  </Text>
+                  <Text
+                    style={{
+                      color: 'white',
+                      fontSize: 15,
+                      fontFamily: 'Montserrat3',
+                      alignSelf: 'center'
+                    }}
+                  >
+                    Max: {item.main.temp_max}ºC
+                  </Text>
+                </View>
+              )}
+              keyExtractor={(weather) => weather.dt_txt.toString()}
+            />
+          </View>
+
+          <CustomButton
+            title="Search"
+            onPress={() => {
+              getWeather(text)
+            }}
+          />
+          <CustomButton
+            title="Save place"
+            onPress={() => {
+              alert('Need to save to db')
+            }}
+          />
         </ScrollView>
-      </View>
+      </ImageBackground>
     )
   }
   function getWeather(text) {
     // useEffect(() => {})
+    // if (text === undefined) {
+    //   alert(result)
+    // }
     const searchApi = async () => {
       try {
         const response = await api.get(
@@ -184,10 +204,9 @@ export default function SearchScreen() {
         setResult(response.data)
         // console.log('forecast log from btn: ', response.data)
         // console.log(result.list.length, 'result list length')
-        return result
       } catch (error) {
         setResult(error.toString())
-        console.log('Error Result from api call', result)
+        alert(result)
       }
     }
     searchApi()
@@ -198,7 +217,6 @@ export default function SearchScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
   },
@@ -210,11 +228,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center'
   },
-  viewList: {
-    // backgroundColor: 'grey',
-    alignItems: 'center',
-    padding: 20
-  },
+  // viewList: {
+  //   alignItems: 'center',
+  //   padding: 20
+  // },
   eachList: {
     margin: 2,
     flex: 1
@@ -223,7 +240,7 @@ const styles = StyleSheet.create({
     margin: 10,
     flex: 0.9,
     borderWidth: 2.5,
-    borderColor: 'black',
+    borderColor: 'lightgrey',
     shadowOffset: {
       width: 5,
       height: 5
@@ -236,6 +253,7 @@ const styles = StyleSheet.create({
 // Style for text in components
 const textStyles = StyleSheet.create({
   Country: {
+    color: 'white',
     fontSize: 30,
     fontFamily: 'Montserrat1',
     padding: 5,
@@ -247,6 +265,7 @@ const textStyles = StyleSheet.create({
     shadowRadius: 2
   },
   state: {
+    color: 'white',
     fontFamily: 'Montserrat2',
     paddingBottom: 20,
     shadowOffset: {
@@ -257,7 +276,7 @@ const textStyles = StyleSheet.create({
     shadowRadius: 2
   },
   feelsLike: {
-    paddingBottom: 20,
+    color: 'white',
     fontFamily: 'Montserrat3',
     fontSize: 15,
     shadowOffset: {
@@ -268,19 +287,31 @@ const textStyles = StyleSheet.create({
     shadowRadius: 2
   },
   description: {
+    color: 'white',
     margin: 10,
     fontSize: 20,
     fontFamily: 'Montserrat1'
   },
   main: {
+    color: 'white',
     margin: 10,
-    fontSize: 18,
+    fontSize: 20,
     fontFamily: 'Montserrat1'
   },
-  max: {
-    fontSize: 16
-  },
   min: {
-    fontSize: 16
+    fontFamily: 'Montserrat1',
+    color: '#fff'
+  },
+  max: {
+    color: '#fff',
+    fontFamily: 'Montserrat1'
+  },
+  humidity: {
+    color: '#fff',
+    fontFamily: 'Montserrat1'
+  },
+  visibility: {
+    color: '#fff',
+    fontFamily: 'Montserrat1'
   }
 })
