2c1c86089f53101a0070adac20114b0f2d464eba
RomanDivkovic <divkovicroman@gmail.com>
2022-01-25 11:41:33 +0100

    Added border to forecast every 3h

--- src/Screens/SearchScreen.js
+++ src/Screens/SearchScreen.js
@@ -6,21 +6,13 @@ import {
   Image,
   ScrollView,
   Alert,
-  FlatList,
-  SectionList
+  FlatList
 } from 'react-native'
 import CustomButton from '../components/customButton'
 import CustomInput from '../components/customInput'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import api from '../Utils/api/api'
 import api_key from '../components/apiKey'
-import backgroundPhoto from '../assets/photo/sunrise.jpg'
-
-/*
-
-Need to display forcast of weather from every 3h at bottom now
-
-*/
 
 export default function SearchScreen() {
   const [result, setResult] = useState([])
@@ -29,8 +21,8 @@ export default function SearchScreen() {
 
   if (loaded === true) {
     return (
-      <SafeAreaView style={{ backgroundColor: '#fff', height: '100%' }}>
-        <ScrollView>
+      <ScrollView>
+        <SafeAreaView>
           <CustomInput onChangeText={onTextChange} value={text} />
           <CustomButton
             title="Search"
@@ -42,8 +34,6 @@ export default function SearchScreen() {
                   )
                   setResult(response.data)
                   setLoaded(false)
-
-                  return result
                 } catch (error) {
                   setResult(error.toString())
                   console.log('Error Result from api call', result)
@@ -53,123 +43,154 @@ export default function SearchScreen() {
               searchApi()
             }}
           />
-        </ScrollView>
-      </SafeAreaView>
+        </SafeAreaView>
+      </ScrollView>
     )
   } else {
-    // const ListItem = ({ item }) => {
-    //   return (
-    //     <View style={styles.item}>
-    //       <Image
-    //         style={styles.pic}
-    //         source={{
-    //           uri: `http://openweathermap.org/img/w/${result.list[0].weather[0].icon}.png`
-    //         }}
-    //       />
-    //       <Text>{item.main.temp}ºC</Text>
-    //     </View>
-    //   )
-    // }
     return (
-      <SafeAreaView style={{ backgroundColor: '#fff', height: '100%' }}>
+      <View style={{ height: '100%' }}>
         <ScrollView>
-          <CustomInput onChangeText={onTextChange} value={text} />
-          <View style={styles.container}>
-            <Text style={textStyles.Country}>
-              {result.city.name}, {result.city.country}
-            </Text>
-            <Text>{result.city.sunrise}</Text>
-            <Text>{result.city.sunset}</Text>
-            <Text
-              style={{
-                fontSize: 22,
-                fontWeight: '700',
-                shadowOffset: {
-                  width: 5,
-                  height: 5
-                },
-                shadowOpacity: 0.4,
-                shadowRadius: 2
-              }}
-            >
-              {result.list[0].main.temp}ºC
-            </Text>
-            <Text tyle={textStyles.feelsLike}>
-              {result.list[0].main.feels_like}ºC
-            </Text>
+          <SafeAreaView>
+            <CustomInput onChangeText={onTextChange} value={text} />
+            <View style={styles.container}>
+              <Text style={textStyles.Country}>
+                {result.city.name}, {result.city.country}
+              </Text>
+              <Text>{result.city.sunrise}</Text>
+              <Text>{result.city.sunset}</Text>
+              <Text
+                style={{
+                  fontSize: 22,
+                  fontWeight: '700',
+                  shadowOffset: {
+                    width: 5,
+                    height: 5
+                  },
+                  shadowOpacity: 0.4,
+                  shadowRadius: 2
+                }}
+              >
+                {result.list[0].main.temp}ºC
+              </Text>
+              <Text tyle={textStyles.feelsLike}>
+                {result.list[0].main.feels_like}ºC
+              </Text>
 
-            <Text style={styles.main}>{result.list[0].weather[0].main}</Text>
-            <Text style={styles.description}>
-              {result.list[0].weather[0].description}
-            </Text>
-            <Image
-              style={styles.pic}
-              source={{
-                uri: `http://openweathermap.org/img/w/${result.list[0].weather[0].icon}.png`
+              <Text style={styles.main}>{result.list[0].weather[0].main}</Text>
+              <Text style={styles.description}>
+                {result.list[0].weather[0].description}
+              </Text>
+              <Image
+                style={styles.pic}
+                source={{
+                  uri: `http://openweathermap.org/img/w/${result.list[0].weather[0].icon}.png`
+                }}
+              />
+              <Text style={styles.max}>
+                Max: {result.list[0].main.temp_max}ºC
+              </Text>
+              <Text style={styles.min}>
+                Min: {result.list[0].main.temp_min}ºC
+              </Text>
+              <Text>Humidity: {result.list[0].main.humidity}</Text>
+              <Text>Visibility: {result.list[0].visibility}</Text>
+            </View>
+
+            <View style={styles.viewList}>
+              <View style={styles.eachList}>
+                <FlatList
+                  data={result.list}
+                  horizontal
+                  renderItem={({ item }) => (
+                    <View style={styles.item}>
+                      <Text style={{ fontFamily: 'Montserrat1' }}>
+                        {item.dt_txt}
+                      </Text>
+                      <Image
+                        style={styles.pic}
+                        source={{
+                          uri: `http://openweathermap.org/img/w/${item.weather[0].icon}.png`
+                        }}
+                      />
+                      <Text
+                        style={{
+                          alignSelf: 'center',
+                          fontFamily: 'Montserrat2'
+                        }}
+                      >
+                        {item.weather[0].description}
+                      </Text>
+                      <Text
+                        style={{
+                          fontSize: 15,
+                          fontFamily: 'Montserrat3',
+                          alignSelf: 'center'
+                        }}
+                      >
+                        Current: {item.main.temp}ºC
+                      </Text>
+                      <Text
+                        style={{
+                          fontSize: 15,
+                          fontFamily: 'Montserrat3',
+                          alignSelf: 'center'
+                        }}
+                      >
+                        Feels like: {item.main.feels_like}ºC
+                      </Text>
+                      <Text
+                        style={{
+                          fontSize: 15,
+                          fontFamily: 'Montserrat3',
+                          alignSelf: 'center'
+                        }}
+                      >
+                        Min: {item.main.temp_min}ºC
+                      </Text>
+                      <Text
+                        style={{
+                          fontSize: 15,
+                          fontFamily: 'Montserrat3',
+                          alignSelf: 'center'
+                        }}
+                      >
+                        Max: {item.main.temp_max}ºC
+                      </Text>
+                    </View>
+                  )}
+                  keyExtractor={(weather) => weather.dt_txt.toString()}
+                />
+              </View>
+            </View>
+
+            <CustomButton
+              title="Search"
+              onPress={() => {
+                getWeather(text)
               }}
             />
-            <Text style={styles.max}>
-              Max: {result.list[0].main.temp_max}ºC
-            </Text>
-            <Text style={styles.min}>
-              Min: {result.list[0].main.temp_min}ºC
-            </Text>
-            {/* <Text>Humidity: {result.list[0].main.humidity}</Text>
-            <Text>Visibility: {result.list[0].visibility}</Text> */}
-          </View>
-
-          <View style={styles.viewList}>
-            <View style={styles.eachList}>
-              <FlatList
-                data={result.list}
-                horizontal
-                renderItem={({ item }) => (
-                  <View style={styles.item}>
-                    <Text>{item.dt_txt}</Text>
-                    <Image
-                      style={styles.pic}
-                      source={{
-                        uri: `http://openweathermap.org/img/w/${item.weather[0].icon}.png`
-                      }}
-                    />
-                    <Text>{item.main.temp}ºC</Text>
-                    <Text>{item.main.feels_like}ºC</Text>
-                    <Text>Minaml temp {item.main.temp_min}ºC</Text>
-                    <Text>
-                      Maximum temp:
-                      {item.main.temp_max}
-                      ºC
-                    </Text>
-                  </View>
-                )}
-                // keyExtractor={(weather) => weather.city.id.toString()}
-              />
-            </View>
-          </View>
-
-          <CustomButton
-            title="Search"
-            onPress={() => {
-              const searchApi = async () => {
-                try {
-                  const response = await api.get(
-                    `forecast?q=${text}&units=metric&appid=${api_key}`
-                  )
-                  setResult(response.data)
-                  console.log('forecast log from btn: ', response.data)
-                  console.log(result.list.length, 'result list length')
-                  return result
-                } catch (error) {
-                  setResult(error.toString())
-                  console.log('Error Result from api call', result)
-                }
-              }
-              searchApi()
-            }}
-          />
+          </SafeAreaView>
         </ScrollView>
-      </SafeAreaView>
+      </View>
     )
+  }
+  function getWeather(text) {
+    // useEffect(() => {})
+    const searchApi = async () => {
+      try {
+        const response = await api.get(
+          `forecast?q=${text}&units=metric&appid=${api_key}`
+        )
+        setResult(response.data)
+        // console.log('forecast log from btn: ', response.data)
+        // console.log(result.list.length, 'result list length')
+        return result
+      } catch (error) {
+        setResult(error.toString())
+        console.log('Error Result from api call', result)
+      }
+    }
+    searchApi()
   }
 }
 
@@ -177,8 +198,7 @@ export default function SearchScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // padding: 20,
-    backgroundColor: '#fff',
+    // backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
   },
@@ -191,13 +211,25 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   viewList: {
-    backgroundColor: 'grey',
+    // backgroundColor: 'grey',
     alignItems: 'center',
     padding: 20
   },
   eachList: {
-    flex: 1,
-    margin: 10
+    margin: 2,
+    flex: 1
+  },
+  item: {
+    margin: 10,
+    flex: 0.9,
+    borderWidth: 2.5,
+    borderColor: 'black',
+    shadowOffset: {
+      width: 5,
+      height: 5
+    },
+    shadowOpacity: 0.8,
+    shadowRadius: 2
   }
 })
 
