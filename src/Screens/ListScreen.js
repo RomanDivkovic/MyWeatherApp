d479cc294914c8ebae952d5616943c2ff941325d
RomanDivkovic <divkovicroman@gmail.com>
2022-01-26 23:28:57 +0100

    can now read data

--- src/Screens/ListScreen.js
+++ src/Screens/ListScreen.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-import { StyleSheet, View, Text, FlatList } from 'react-native'
+import { StyleSheet, View, Text, FlatList, ImageBackground } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import * as firebase from 'firebase'
 
 /*
 LEFT TO DO IS FIX THE BACKGROUND IN ON THE APP AND ALSO IMPLIMENT FIREBASE OR SOMETHING SO USER CAN SAVE LOCATION AND IN THE LIST SCREEN CALL SAVED LOCATIONS TO LOOP THRU AND SHOWING THEM WITH NAME, TEMP AND ICON
@@ -8,19 +9,61 @@ LEFT TO DO IS FIX THE BACKGROUND IN ON THE APP AND ALSO IMPLIMENT FIREBASE OR SO
 
 export default function ListScreen(props) {
   const savedCity = props.route.params
+  const image = {
+    uri: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_960_720.jpg'
+  }
+
+  // Get's the city saved
+  firebase
+    .database()
+    .ref('user')
+    .on('value', (snapshot) => {
+      const city = snapshot.val().weather
+      console.log('City: ' + city)
+    })
+
+  // const dbRef = firebase.database().ref()
+  // dbRef
+  //   .child('user')
+  //   .child(userId)
+  //   .get()
+  //   .then((snapshot) => {
+  //     if (snapshot.exists()) {
+  //       console.log(snapshot.val())
+  //     } else {
+  //       console.log('No data available')
+  //     }
+  //   })
+  //   .catch((error) => {
+  //     console.error(error)
+  //   })
 
   return (
-    <SafeAreaView>
-      <View>
+    <SafeAreaView style={styles.container}>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <Text style={styles.text}>Inside</Text>
+      </ImageBackground>
+      {/* <View>
         <Text style={styles.Text}>Listscreen</Text>
-      </View>
+      </View> */}
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  Text: {
-    fontSize: 20,
+  container: {
+    flex: 1
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  text: {
+    color: 'white',
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: 'bold',
     textAlign: 'center'
+    // backgroundColor: '#000000c0'
   }
 })
